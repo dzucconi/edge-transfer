8736b25987fae3d051ffe8b2f02e2df225bdfeef
dzucconi <mail@damonzucconi.com>
2020-10-04 11:58:40 -0400

    Converts project; updates data

---
+++ src/data.ts
@@ -0,0 +1,62 @@
+// https://glyph.labs.auspic.es/graph/b307b11a-ea6a-4a4a-8259-5a9d65387ada
+
+type Sets = {
+  [key: string]: {
+    name: string;
+    images: {
+      L: string;
+      R: string;
+    };
+  };
+};
+
+export const SETS: Sets = {
+  chairs: {
+    name: "Chairs",
+    images: {
+      L:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiNTcxNS9jZGE1NjkwZWVlZWNkYzY3MjA0NDlkZjMwZmI5ZWI2ODU1NGE0ODc1NjBhNTEyNGFiNzkwMGJmZDhmMGMwOGM5LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTY0MCwiaGVpZ2h0IjoxNjQwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+      R:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiNTcxNC9jY2U5M2Q1NjQzOWNkOGI1MDVjZWUyMmQzNWZhNjlhMTRiNTdlZDE0Y2Y4YzljZTkwZjU1MGE0MTY4NTQ5N2E3LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTY0MCwiaGVpZ2h0IjoxNjQwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+    },
+  },
+  "sea-wall": {
+    name: "Sea-wall at Scarborough",
+    images: {
+      L:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiNTE5NC82OTZjYTg2YmNjYTIxNWM3NjY1MTlkZGRkMzc4OTkzMmRmN2UyZjI0OGU1OTM2ODFhNmZjY2Q1NzFjMTYzMTU4LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTAwMCwiaGVpZ2h0Ijo3NTAsImZpdCI6Imluc2lkZSJ9LCJ3ZWJwIjp7InF1YWxpdHkiOjc1fSwianBlZyI6eyJxdWFsaXR5Ijo3NX0sInJvdGF0ZSI6bnVsbH19",
+      R:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiNTE5NS8xNjFmODVmMzBiNzkxMjJkMmUwMTA5ZTExN2UyNGYzNDYyNWEyNzY2N2QxMmRiYWI4YzAxZWViZGQ1OTllNGY1LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTg4MiwiaGVpZ2h0IjoxNjM0LCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+    },
+  },
+
+  "draft-pistons": {
+    name: "Draft Pistons",
+    images: {
+      L:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS8wOHkwb2dsQmdxbUhMZmZqLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjE4NiwiaGVpZ2h0IjoyNTYwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+      R:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS9kcFV1eWg4eFF1Z3I5WkoxLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjE1MiwiaGVpZ2h0IjoyNTYwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+    },
+  },
+
+  "bentheim-castle-and-landschaft": {
+    name: "Bentheim Castle & Landschaft",
+    images: {
+      L:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS96Zkx4ZzhnSUdEQ2FrUzhHLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjEyMCwiaGVpZ2h0IjoxNjAwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+      R:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS9NdFBFNkpIaXBtclNibmNkLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjU2MCwiaGVpZ2h0IjoyMDQ4LCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+    },
+  },
+
+  "rhea-and-dione": {
+    name: "Rhea & Dione",
+    images: {
+      L:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS9EUU5KVEZXN213ODFVanNBLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjU2MCwiaGVpZ2h0IjoyNTYwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+      R:
+        "https://dynamic.atlas.auspic.es/eyJidWNrZXQiOiJhdGxhcy1wcm9kdWN0aW9uIiwia2V5IjoiMS81djJSTWxoZDRSZmplMXlJLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjUwOCwiaGVpZ2h0IjoyNTYwLCJmaXQiOiJpbnNpZGUifSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==",
+    },
+  },
+};
